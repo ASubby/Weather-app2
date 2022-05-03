7601f6bd79ab645d5fdeab0d754bf2753e02a7c1
ASubby <104353408+ASubby@users.noreply.github.com>
2022-05-03 11:31:27 +0100

    Created Search Engine

--- Src/index.js
+++ Src/index.js
@@ -23,28 +23,19 @@ function formatDate(date) {
   return `${day} ${currentHour}:${currentMinutes}`;
 }
 
-function displayWeatherCondition(response) {
+function displayWeatherCondition() {
   let h1 = document.querySelector("#city");
   h1.innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
 }
-function searchCity(city) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+function search(event) {
+  event.preventDefault();
   let apiKey = `749717f0f4a2e36ed817dec27de38882`;
+  let cityInput = document.querySelector("#city-input");
+  let city = cityInput.value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
-}
-
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
-
-let cityForm = document.querySelector("#city-form");
-cityForm.addEventListener("submit", handleSubmit);
